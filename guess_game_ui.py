1e07b1f9302d1baadd94260e4989962c63427dc1
manonmani-2007 <manonmani1009.cse@gmail.com>
2026-01-11 10:09:05 +0530

    Add files via upload

---
+++ guess_game_ui.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import random
+
+st.set_page_config(page_title="Guess The Number", page_icon="🎯")
+st.title("🎯 Guess The Number Game")
+st.sidebar.header("🎯Difficulty Level")
+difficulty=st.sidebar.radio("Choose Level:",["🟢Easy","🟡Medium","🔴Hard"])
+if difficulty=="🟢Easy":
+    max_range=50
+    value=15
+elif difficulty=="🟡Medium":
+    max_range=100
+    value=10
+else:
+    max_range=200
+    value=7
+if "secret" not in st.session_state:
+    st.session_state.secret = random.randint(1, max_range)
+    st.session_state.attempts = 0
+    st.session_state.max_attempts=value
+    st.session_state.game_over=False
+if st.session_state.max_attempts!=value:
+     st.session_state.max_attempts=value
+     st.session_state.secret=random.randint(1,max_range)
+     st.session_state.attempts=0
+     st.session_state.game_over=False
+st.sidebar.header("🎮Game Info")
+st.sidebar.write("Guess the Secret Number👀")
+st.sidebar.write(f"Range 1-{max_range}")
+st.sidebar.write(f"MAximum Attempts:{st.session_state.max_attempts}")
+st.write(f"I have chosen a number between 1 and {max_range}")
+
+c1,c2,c3=st.columns([1,2,1])
+with c2:
+    guess = st.number_input(
+    "Enter your guess:",
+    min_value=1,
+    max_value=max_range,
+    step=1
+)
+    
+    left=st.session_state.max_attempts-st.session_state.attempts
+    st.info(f"⌛Attempts Left:{left}")
+    if st.button("🎲 Guess",use_container_width=True) and not st.session_state.game_over:
+        st.session_state.attempts += 1
+        if guess < st.session_state.secret:
+            st.warning("🔽 Too low")
+        elif guess > st.session_state.secret:
+            st.warning("🔼 Too high")
+        else:
+            st.success("🎉 Correct!")
+            st.write(f"You guessed it in {st.session_state.attempts} attempts")
+            st.balloons()
+            st.session_state.game_over=True
+        if st.session_state.attempts>st.session_state.max_attempts and not st.session_state.game_over:
+            st.error("😓Maximum Attempts Reached")
+            st.write(f"The secret number was {st.session_state.secret}")
+            st.session_state.game_over=True 
+    st.divider()
+    if st.button("🔄️ Restart Game"):
+        st.session_state.secret = random.randint(1, 100)
+        st.session_state.attempts = 0
+        st.session_state.game_over=False
+        st.success("New Game Restarted!🎮")
+    
